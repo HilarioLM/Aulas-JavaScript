cbe94d02d9a5652b57fa3c4bfdd708b7f4ecc37b
Hilário Lelis <65120763+HilarioLM@users.noreply.github.com>
2020-05-17 08:32:49 -0300

    Exercício 16

--- Aula14/ex016/script.js
+++ Aula14/ex016/script.js
@@ -2,18 +2,31 @@ function contador() {
     var inicio = document.getElementById("inicio")
     var fim = document.getElementById("fim")
     var passo = document.getElementById("passo")
-    var res = document.querySelector('div#res')
+    var res = document.getElementById('res')
     
-    if (inicio.value.length == '') {
-        alert('Impossivel Calcular, por favor digite um valor')
-    } else if (inicio < fim) {
-        for (var cont = inicio; cont <= fim; cont += passo){
-            console.log(cont)
-         } 
+    if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        res.innerHTML = 'Impossível contar!'
     } else {
-        for (var cont = fim; cont >= inicio; cont -= passo){
-            console.log(cont)
+        res.innerHTML = 'Contanto: '
+        var ini = Number(inicio.value)
+        var fi = Number(fim.value)
+        var pas = Number(passo.value)
+        if (pas == 0) {
+            alert('Passo invalido, considerando PASSO 1')
+            pas = 1
         }
-        res.getElementById(cont) 
-    }  
+
+        if (ini < fi) {
+            for( var cont = ini; cont <= fi; cont += pas) {
+                res.innerHTML += `${cont} `
+            }
+        } else {
+            for(var cont = ini; cont >= i; cont -= pas) {
+                res.innerHTML += `${cont}`
+            }
+        }
+        res.innerHTML += ''
+    }
+        
+
 }
